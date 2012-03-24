8e166123749788d6d1d14652af7309582bfce50e
David Dodge <se5zeziroth@gmail.com>
2012-03-24 12:36:18 -0400

    Added beginning of dashboard w/sliders

--- Giv2GivAndroid/gen/giv2giv/android/R.java
+++ Giv2GivAndroid/gen/giv2giv/android/R.java
@@ -13,8 +13,22 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int conservativeLabel=0x7f050001;
+        public static final int conservativeLock=0x7f050004;
+        public static final int conservativeSlider=0x7f050002;
+        public static final int liberalLabel=0x7f050008;
+        public static final int liberalLock=0x7f05000a;
+        public static final int liberalSlider=0x7f050009;
+        public static final int lockLabel=0x7f050003;
+        public static final int moderateLabel=0x7f050005;
+        public static final int moderateLock=0x7f050007;
+        public static final int moderateSlider=0x7f050006;
+        public static final int sliderBank=0x7f050000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int dashboard=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- Giv2GivAndroid/src/giv2giv/android/Giv2GivAndroidActivity.java
+++ Giv2GivAndroid/src/giv2giv/android/Giv2GivAndroidActivity.java
@@ -8,6 +8,6 @@ public class Giv2GivAndroidActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.dashboard);
     }
 }
