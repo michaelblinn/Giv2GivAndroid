67a1c900feb5319f24011cbf5c164213b584876d
David Dodge <se5zeziroth@gmail.com>
2012-03-24 15:30:46 -0400

    How to Invest chunk of dashboard is finished

--- Giv2GivAndroid/gen/giv2giv/android/R.java
+++ Giv2GivAndroid/gen/giv2giv/android/R.java
@@ -14,17 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int conservativeDollar=0x7f050007;
         public static final int conservativeLabel=0x7f050002;
         public static final int conservativeLock=0x7f050005;
+        public static final int conservativePercent=0x7f050006;
         public static final int conservativeSlider=0x7f050003;
         public static final int currentFundTotal=0x7f050001;
-        public static final int liberalLabel=0x7f050009;
-        public static final int liberalLock=0x7f05000b;
-        public static final int liberalSlider=0x7f05000a;
+        public static final int liberalDollar=0x7f050011;
+        public static final int liberalLabel=0x7f05000d;
+        public static final int liberalLock=0x7f05000f;
+        public static final int liberalPercent=0x7f050010;
+        public static final int liberalSlider=0x7f05000e;
         public static final int lockLabel=0x7f050004;
-        public static final int moderateLabel=0x7f050006;
-        public static final int moderateLock=0x7f050008;
-        public static final int moderateSlider=0x7f050007;
+        public static final int moderateDollar=0x7f05000c;
+        public static final int moderateLabel=0x7f050008;
+        public static final int moderateLock=0x7f05000a;
+        public static final int moderatePercent=0x7f05000b;
+        public static final int moderateSlider=0x7f050009;
         public static final int sliderBank=0x7f050000;
     }
     public static final class layout {
--- Giv2GivAndroid/src/giv2giv/android/Giv2GivAndroidActivity.java
+++ Giv2GivAndroid/src/giv2giv/android/Giv2GivAndroidActivity.java
@@ -1,7 +1,12 @@
 package giv2giv.android;
 
+import java.math.BigDecimal;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -12,6 +17,9 @@ public class Giv2GivAndroidActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.dashboard);
+        final double ourMoney = 87;
+        TextView currentFundTotal = (TextView)findViewById(R.id.currentFundTotal);
+        currentFundTotal.setText(currentFundTotal.getText() + "" + ourMoney);
         SeekBar conservativeSlider = (SeekBar)findViewById(R.id.conservativeSlider);
         conservativeSlider.setProgress(34);
         conservativeSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() 
@@ -25,7 +33,6 @@ public class Giv2GivAndroidActivity extends Activity {
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
@@ -36,48 +43,60 @@ public class Giv2GivAndroidActivity extends Activity {
 				{
 					return;
 				}
-				System.out.println(seekBar.getProgress());
-				System.out.println(progress);
 				int otherProgress = 100-progress;
-		    	switch (seekBar.getId())
-		    	{
-		    	case R.id.conservativeSlider:
-		    		if (otherProgress % 2 != 0)
-		    		{
-		    			int medProgress = (otherProgress / 2) + 1;
-		    			int highProgress = medProgress - 1;
-		    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
-		    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
-		    			medSeek.setProgress(medProgress);
-		    			highSeek.setProgress(highProgress);
-		    		}
-		    		break;
-		    		
-		    	case R.id.moderateSlider:
-					if (otherProgress % 2 != 0)
-					{
-						int lowProgress = (otherProgress / 2) + 1;
-						int highProgress = lowProgress - 1;
-		    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
-		    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
-		    			lowSeek.setProgress(lowProgress);
-		    			highSeek.setProgress(highProgress);
-		    			
-					}
-		    		break;
-		    		
-		    	case R.id.liberalSlider:
-		    		if (otherProgress % 2 != 0)
-		    		{
-		    			int lowProgress = (otherProgress / 2) + 1;
-		    			int medProgress = lowProgress - 1;
-		    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
-		    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
-		    			lowSeek.setProgress(lowProgress);
-		    			medSeek.setProgress(medProgress);
-		    		}
-		    		break;
-		    	}
+				TextView consPerc = (TextView)findViewById(R.id.conservativePercent);
+    			TextView medPerc = (TextView)findViewById(R.id.moderatePercent);
+    			TextView highPerc = (TextView)findViewById(R.id.liberalPercent);
+    			TextView consDolla = (TextView)findViewById(R.id.conservativeDollar);
+    			TextView modDolla = (TextView)findViewById(R.id.moderateDollar);
+    			TextView libDolla = (TextView)findViewById(R.id.liberalDollar);
+    			int medProgress = (otherProgress / 2);
+    			int highProgress = medProgress;
+    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
+    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
+    			if (!medSeek.isEnabled() && !highSeek.isEnabled())
+    			{
+    				medProgress = medSeek.getProgress();
+    				highProgress = highSeek.getProgress();
+    				seekBar.setProgress(100 - medProgress - highProgress);
+        			consPerc.setText("" + seekBar.getProgress() + "%");
+    			}
+    			else if (!medSeek.isEnabled())
+    			{
+    				medProgress = medSeek.getProgress();
+    				if (progress > (100 - medProgress))
+    				{
+    					seekBar.setProgress(100 - medProgress);
+    				}
+    				highSeek.setProgress(100 - (progress + medProgress));
+        			consPerc.setText("" + seekBar.getProgress() + "%");
+        			highPerc.setText("" + highSeek.getProgress() + "%");
+    			}
+    			else if (!highSeek.isEnabled())
+    			{
+    				highProgress = highSeek.getProgress();
+    				if (progress > (100 - highProgress))
+    				{
+    					seekBar.setProgress(100 - highProgress);
+    				}
+    				medSeek.setProgress(100 - (progress + highProgress));
+        			consPerc.setText("" + seekBar.getProgress() + "%");
+        			medPerc.setText("" + medSeek.getProgress() + "%");
+    			}
+    			else
+    			{
+        			medSeek.setProgress(medProgress);
+        			highSeek.setProgress(highProgress);
+        			consPerc.setText("" + seekBar.getProgress() + "%");
+        			medPerc.setText("" + medSeek.getProgress() + "%");
+        			highPerc.setText("" + highSeek.getProgress() + "%");
+    			}
+    			double percent = ((double)seekBar.getProgress()) / 100;
+    			consDolla.setText("=$" + (round(percent * ourMoney, 2)));
+    			percent = ((double)medSeek.getProgress()) / 100;
+    			modDolla.setText("=$" + (round(percent * ourMoney,2)));
+    			percent = ((double)highSeek.getProgress()) / 100;
+    			libDolla.setText("=$" + (round(percent * ourMoney,2)));
 			}
 		});
         SeekBar moderateSlider = (SeekBar)findViewById(R.id.moderateSlider);
@@ -105,45 +124,59 @@ public class Giv2GivAndroidActivity extends Activity {
 					return;
 				}
 				int otherProgress = 100-progress;
-		    	switch (seekBar.getId())
-		    	{
-		    	case R.id.conservativeSlider:
-		    		if (otherProgress % 2 != 0)
-		    		{
-		    			int medProgress = (otherProgress / 2) + 1;
-		    			int highProgress = medProgress - 1;
-		    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
-		    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
-		    			medSeek.setProgress(medProgress);
-		    			highSeek.setProgress(highProgress);
-		    		}
-		    		break;
-		    		
-		    	case R.id.moderateSlider:
-					if (otherProgress % 2 != 0)
-					{
-						int lowProgress = (otherProgress / 2) + 1;
-						int highProgress = lowProgress - 1;
-		    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
-		    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
-		    			lowSeek.setProgress(lowProgress);
-		    			highSeek.setProgress(highProgress);
-		    			
-					}
-		    		break;
-		    		
-		    	case R.id.liberalSlider:
-		    		if (otherProgress % 2 != 0)
-		    		{
-		    			int lowProgress = (otherProgress / 2) + 1;
-		    			int medProgress = lowProgress - 1;
-		    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
-		    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
-		    			lowSeek.setProgress(lowProgress);
-		    			medSeek.setProgress(medProgress);
-		    		}
-		    		break;
-		    	}
+				TextView consPerc = (TextView)findViewById(R.id.conservativePercent);
+    			TextView medPerc = (TextView)findViewById(R.id.moderatePercent);
+    			TextView highPerc = (TextView)findViewById(R.id.liberalPercent);
+    			TextView consDolla = (TextView)findViewById(R.id.conservativeDollar);
+    			TextView modDolla = (TextView)findViewById(R.id.moderateDollar);
+    			TextView libDolla = (TextView)findViewById(R.id.liberalDollar);
+				int lowProgress = (otherProgress / 2);
+				int highProgress = lowProgress;
+    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
+    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
+    			if (!lowSeek.isEnabled() && !highSeek.isEnabled())
+    			{
+    				lowProgress = lowSeek.getProgress();
+    				highProgress = highSeek.getProgress();
+    				seekBar.setProgress(100 - lowProgress - highProgress);
+        			medPerc.setText("" + seekBar.getProgress() + "%");
+    			}
+    			else if (!lowSeek.isEnabled())
+    			{
+    				lowProgress = lowSeek.getProgress();
+    				if (progress > (100 - lowProgress))
+    				{
+    					seekBar.setProgress(100 - lowProgress);
+    				}
+    				highSeek.setProgress(100 - (progress + lowProgress));
+        			medPerc.setText("" + seekBar.getProgress() + "%");
+        			highPerc.setText("" + highSeek.getProgress() + "%");
+    			}
+    			else if (!highSeek.isEnabled())
+    			{
+    				highProgress = highSeek.getProgress();
+    				if (progress > (100 - highProgress))
+    				{
+    					seekBar.setProgress(100 - highProgress);
+    				}
+    				lowSeek.setProgress(100 - (progress + highProgress));
+        			medPerc.setText("" + seekBar.getProgress() + "%");
+        			consPerc.setText("" + lowSeek.getProgress() + "%");
+    			}
+    			else
+    			{
+        			lowSeek.setProgress(lowProgress);
+        			highSeek.setProgress(highProgress);
+        			medPerc.setText("" + seekBar.getProgress() + "%");
+        			consPerc.setText("" + lowSeek.getProgress() + "%");
+        			highPerc.setText("" + highSeek.getProgress() + "%");
+    			}
+    			double percent = ((double)lowSeek.getProgress()) / 100;
+    			consDolla.setText("=$" + (round(percent * ourMoney, 2)));
+    			percent = ((double)seekBar.getProgress()) / 100;
+    			modDolla.setText("=$" + (round(percent * ourMoney,2)));
+    			percent = ((double)highSeek.getProgress()) / 100;
+    			libDolla.setText("=$" + (round(percent * ourMoney,2)));
 			}
 		});
         SeekBar liberalSlider = (SeekBar)findViewById(R.id.liberalSlider);
@@ -171,52 +204,159 @@ public class Giv2GivAndroidActivity extends Activity {
 					return;
 				}
 				int otherProgress = 100-progress;
-		    	switch (seekBar.getId())
-		    	{
-		    	case R.id.conservativeSlider:
-		    		if (otherProgress % 2 != 0)
-		    		{
-		    			int medProgress = (otherProgress / 2) + 1;
-		    			int highProgress = medProgress - 1;
-		    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
-		    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
-		    			medSeek.setProgress(medProgress);
-		    			highSeek.setProgress(highProgress);
-		    		}
-		    		break;
-		    		
-		    	case R.id.moderateSlider:
-					if (otherProgress % 2 != 0)
-					{
-						int lowProgress = (otherProgress / 2) + 1;
-						int highProgress = lowProgress - 1;
-		    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
-		    			SeekBar highSeek = (SeekBar)findViewById(R.id.liberalSlider);
-		    			lowSeek.setProgress(lowProgress);
-		    			highSeek.setProgress(highProgress);
-		    			
-					}
-		    		break;
-		    		
-		    	case R.id.liberalSlider:
-		    		if (otherProgress % 2 != 0)
-		    		{
-		    			int lowProgress = (otherProgress / 2) + 1;
-		    			int medProgress = lowProgress - 1;
-		    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
-		    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
-		    			lowSeek.setProgress(lowProgress);
-		    			medSeek.setProgress(medProgress);
-		    		}
-		    		break;
-		    	}
+				TextView consPerc = (TextView)findViewById(R.id.conservativePercent);
+    			TextView medPerc = (TextView)findViewById(R.id.moderatePercent);
+    			TextView highPerc = (TextView)findViewById(R.id.liberalPercent);
+    			TextView consDolla = (TextView)findViewById(R.id.conservativeDollar);
+    			TextView modDolla = (TextView)findViewById(R.id.moderateDollar);
+    			TextView libDolla = (TextView)findViewById(R.id.liberalDollar);
+    			int lowProgress = (otherProgress / 2);
+    			int medProgress = lowProgress;
+    			SeekBar lowSeek = (SeekBar)findViewById(R.id.conservativeSlider);
+    			SeekBar medSeek = (SeekBar)findViewById(R.id.moderateSlider);
+    			if (!medSeek.isEnabled() && !lowSeek.isEnabled())
+    			{
+    				lowProgress = lowSeek.getProgress();
+    				medProgress = medSeek.getProgress();
+    				seekBar.setProgress(100 - lowProgress - medProgress);
+        			highPerc.setText("" + seekBar.getProgress() + "%");
+    			}
+    			else if (!medSeek.isEnabled())
+    			{
+    				medProgress = medSeek.getProgress();
+    				if (progress > (100 - medProgress))
+    				{
+    					seekBar.setProgress(100 - medProgress);
+    				}
+    				lowSeek.setProgress(100 - (progress + medProgress));
+        			highPerc.setText("" + seekBar.getProgress() + "%");
+        			consPerc.setText("" + lowSeek.getProgress() + "%");
+    			}
+    			else if (!lowSeek.isEnabled())
+    			{
+    				lowProgress = lowSeek.getProgress();
+    				if (progress > (100 - lowProgress))
+    				{
+    					seekBar.setProgress(100 - lowProgress);
+    				}
+    				medSeek.setProgress(100 - (progress + lowProgress));
+        			highPerc.setText("" + seekBar.getProgress() + "%");
+        			medPerc.setText("" + medSeek.getProgress() + "%");
+    			}
+    			else
+    			{
+        			medSeek.setProgress(medProgress);
+        			lowSeek.setProgress(lowProgress);
+        			highPerc.setText("" + seekBar.getProgress() + "%");
+        			consPerc.setText("" + lowSeek.getProgress() + "%");
+        			medPerc.setText("" + medSeek.getProgress() + "%");
+    			}
+    			double percent = ((double)lowSeek.getProgress()) / 100;
+    			consDolla.setText("=$" + (round(percent * ourMoney, 2)));
+    			percent = ((double)medSeek.getProgress()) / 100;
+    			modDolla.setText("=$" + (round(percent * ourMoney,2)));
+    			percent = ((double)seekBar.getProgress()) / 100;
+    			libDolla.setText("=$" + (round(percent * ourMoney,2)));
 			}
 		});
+        
         TextView conservativeValue = (TextView)findViewById(R.id.conservativeLabel);
-        //conservativeValue.setText("" + conservativeSlider.getProgress());
         TextView moderateValue = (TextView)findViewById(R.id.moderateLabel);
-        //moderateValue.setText("" + moderateSlider.getProgress());
         TextView liberalValue = (TextView)findViewById(R.id.liberalLabel);
-        //liberalValue.setText("" + liberalSlider.getProgress());
+        
+        TextView conservativePerc = (TextView)findViewById(R.id.conservativePercent);
+        conservativePerc.setText("" + conservativeSlider.getProgress() + "%");
+        TextView moderatePerc = (TextView)findViewById(R.id.moderatePercent);
+        moderatePerc.setText("" + moderateSlider.getProgress() + "%");
+        TextView liberalPerc = (TextView)findViewById(R.id.liberalPercent);
+        liberalPerc.setText("" + liberalSlider.getProgress() + "%");
+        
+        CheckBox consLock = (CheckBox)findViewById(R.id.conservativeLock);
+        consLock.setOnCheckedChangeListener(new OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                SeekBar consSlider = (SeekBar)findViewById(R.id.conservativeSlider);
+                if (consSlider.isEnabled())
+                {
+                	CheckBox modLock = (CheckBox)findViewById(R.id.moderateLock);
+                	CheckBox libLock = (CheckBox)findViewById(R.id.liberalLock);
+                	SeekBar modSeek = (SeekBar)findViewById(R.id.moderateSlider);
+                	SeekBar libSeek = (SeekBar)findViewById(R.id.liberalSlider);
+                	if (modLock.isChecked() || libLock.isChecked())
+                	{
+                		modLock.setChecked(true);
+                		modSeek.setEnabled(false);
+                		libLock.setChecked(true);
+                		libSeek.setEnabled(false);
+                	}
+                	consSlider.setEnabled(false);
+                }
+                else
+                {
+                	consSlider.setEnabled(true);
+                }
+            }
+        });
+        CheckBox modLock = (CheckBox)findViewById(R.id.moderateLock);
+        modLock.setOnCheckedChangeListener(new OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                SeekBar modSlider = (SeekBar)findViewById(R.id.moderateSlider);
+                if (modSlider.isEnabled())
+                {
+                	CheckBox consLock = (CheckBox)findViewById(R.id.conservativeLock);
+                	CheckBox libLock = (CheckBox)findViewById(R.id.liberalLock);
+                	SeekBar consSeek = (SeekBar)findViewById(R.id.conservativeSlider);
+                	SeekBar libSeek = (SeekBar)findViewById(R.id.liberalSlider);
+                	if (consLock.isChecked() || libLock.isChecked())
+                	{
+                		consLock.setChecked(true);
+                		consSeek.setEnabled(false);
+                		libLock.setChecked(true);
+                		libSeek.setEnabled(false);
+                	}
+                	modSlider.setEnabled(false);
+                }
+                else
+                {
+                	modSlider.setEnabled(true);
+                }
+            }
+        });
+        CheckBox libLock = (CheckBox)findViewById(R.id.liberalLock);
+        libLock.setOnCheckedChangeListener(new OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                SeekBar libSlider = (SeekBar)findViewById(R.id.liberalSlider);
+                if (libSlider.isEnabled())
+                {
+                	CheckBox modLock = (CheckBox)findViewById(R.id.moderateLock);
+                	CheckBox consLock = (CheckBox)findViewById(R.id.conservativeLock);
+                	SeekBar modSeek = (SeekBar)findViewById(R.id.moderateSlider);
+                	SeekBar consSeek = (SeekBar)findViewById(R.id.conservativeSlider);
+                	if (modLock.isChecked() || consLock.isChecked())
+                	{
+                		modLock.setChecked(true);
+                		modSeek.setEnabled(false);
+                		consLock.setChecked(true);
+                		consSeek.setEnabled(false);
+                	}
+                	libSlider.setEnabled(false);
+                }
+                else
+                {
+                	libSlider.setEnabled(true);
+                }
+            }
+        });
+    }
+    public static double round(double unrounded, int precision)
+    {
+        BigDecimal bd = new BigDecimal(unrounded);
+        BigDecimal rounded = bd.setScale(precision, BigDecimal.ROUND_DOWN);
+        return rounded.doubleValue();
     }
 }
