1a480cf411396e28493334b3f2e078b45f79ce1e
David Dodge <se5zeziroth@gmail.com>
2012-03-24 11:25:37 -0400

    adding project

---
+++ Giv2GivAndroid/src/giv2giv/android/Giv2GivAndroidActivity.java
@@ -0,0 +1,13 @@
+package giv2giv.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Giv2GivAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
